7670505a87fbcf0c0fb12e130b0c8d47932da27e
mohmedEldeeb <eldeebmahammed@gmail.com>
2024-01-06 07:55:41 +0200

    input filed and button submit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import InputFiled from "./components/InputFiled";
 
 import "./App.css";
 
+export interface Todo {
+  id: number;
+  todo: string;
+  isDone: boolean;
+}
+
 function App() {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handelSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setTodos([...todos, { id: Math.random(), isDone: false, todo }]);
+  };
+
   return (
     <>
       <div className="App">
         <span className="heading">Todo List</span>
+        <InputFiled handelSubmit={handelSubmit} setTodo={setTodo} todo={todo} />
       </div>
     </>
   );
